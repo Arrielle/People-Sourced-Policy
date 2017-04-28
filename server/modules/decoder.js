b2d275a4b4b6cb9cf52b7e5788f687ab2bc0013b
Arrielle Kooiman <arrielle.kooiman@gmail.com>
2017-04-28 09:32:35 -0500

    firebase stuff

--- server/modules/decoder.js
+++ server/modules/decoder.js
@@ -4,12 +4,26 @@ var admin = require("firebase-admin");
 // var logger = require('./logger');
 
 
-
-
 admin.initializeApp({
- credential: admin.credential.cert("./server/firebase-service-account.json"),
- databaseURL: "https://psp-group.firebaseio.com/", // replace this line with your URL
+  credential: admin.credential.cert({
+    "type": process.env.FIREBASE_SERVICE_ACCOUNT_TYPE,
+    "project_id": process.env.FIREBASE_SERVICE_ACCOUNT_PROJECT_ID,
+    "private_key_id": process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY,
+    "client_email": process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_EMAIL,
+    "client_id": process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_ID,
+    "auth_uri": process.env.FIREBASE_SERVICE_ACCOUNT_AUTH_URI,
+    "token_uri": process.env.FIREBASE_SERVICE_ACCOUNT_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.FIREBASE_SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_X509_CERT_URL
+  }),
+  databaseURL: "https://psp-group.firebaseio.com/"
 });
+
+// admin.initializeApp({
+//  credential: admin.credential.cert("./server/firebase-service-account.json"),
+//  databaseURL: "https://psp-group.firebaseio.com/", // replace this line with your URL
+// });
 /* This is where the magic happens. We pull the id_token off of the request,
 verify it against our firebase service account private_key.
 Then we add the decodedToken */
